89f2bcae7261123b9b37684a5a663bef8e6f0168
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-16 11:59:52 -0300

    Remove memoization

--- react/Static.tsx
+++ react/Static.tsx
@@ -1,8 +1,5 @@
-import { memo } from 'react'
 import { injectIntl } from 'react-intl'
 
 import { RichText } from './index'
 
-export default injectIntl(
-  memo(RichText)
-)
+export default injectIntl(RichText)
